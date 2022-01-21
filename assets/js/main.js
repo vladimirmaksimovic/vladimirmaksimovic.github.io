7c5277f678a162bdbcbbc4c6163932f01951d6ad
VM <vmaksimovic.cr34@gmail.com>
2022-01-21 22:46:29 +0100

    style active main navbar link

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,2 +1,14 @@
-/* test */
-console.log("Hello World!");
+// Get main navbar links
+main_navbar_links = document.querySelectorAll(".main-navbar > li a");
+
+// get page path
+const page_path = window.location.pathname;
+
+// Check page path and change color of the link
+if (page_path === "/index.html") {
+  main_navbar_links[0].style.color = "blue";
+} else if (page_path === "/pages/about.html") {
+  main_navbar_links[1].style.color = "blue";
+} else if (page_path === "/pages/reminder.html") {
+  main_navbar_links[2].style.color = "blue";
+}
