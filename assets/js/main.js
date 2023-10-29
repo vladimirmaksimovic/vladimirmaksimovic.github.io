c108ed9ac254a0e8fa838b71413f9e3a07aa1cc0
vladimirmaksimovic <vmaksimovic.cr34@gmail.com>
2023-10-29 19:51:52 +0100

    adding content

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,14 +1,18 @@
 // Get main navbar links
-main_navbar_links = document.querySelectorAll(".main-navbar > li a");
+const brand_link = document.querySelector(".brand > li a");
+const navbar_links = document.querySelectorAll(".main-navbar > li a");
+
+console.log(brand_link);
+console.log(navbar_links);
 
 // get page path
 const page_path = window.location.pathname;
 
 // Check page path and change color of the link
 if (page_path === "/index.html") {
-  main_navbar_links[0].style.color = "blue";
+  brand_link.style.color = "blue";
 } else if (page_path === "/pages/about.html") {
-  main_navbar_links[1].style.color = "blue";
+  header_links[1].style.color = "blue";
 } else if (page_path === "/pages/reminder.html") {
-  main_navbar_links[2].style.color = "blue";
+  header_links[2].style.color = "blue";
 }
